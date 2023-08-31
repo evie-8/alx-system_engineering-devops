a719897decae2df27a8efe59f85e796d6f801e58
evie-8 <neouma29@gmail.com>
2023-08-31 14:09:17 +0300

    using puppet to cread a config file

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+# using a puppet script to create ssh config file
+
+file_line { 'identity':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config'
+  line   => 'IdentityFile ~/.ssh/school',
+}
+
+file_line { 'password':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => 'PasswordAuthentication no',
+}

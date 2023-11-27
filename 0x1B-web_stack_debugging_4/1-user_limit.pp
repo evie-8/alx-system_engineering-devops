68947aa8c79f2837abea77966912f02232c7edbc
evie-8 <neouma29@gmail.com>
2023-11-27 14:45:55 +0300

    correcting identation

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,12 +1,12 @@
 # fixing login errors 
 exec { 'Corrections-1':
-  command  => 'sudo sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
-  path => '/usr/local/bin/:/bin/'
+  command => 'sudo sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
 
 }
 
 exec { 'soft-file-limit-increase':
-  command  => 'sudo sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
-  path => '/usr/local/bin/:/bin/',
+  command => 'sudo sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/',
 
 }
